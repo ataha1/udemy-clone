325b85c9ba909616a3d721010e9232492a790e43
ataha1 <a.taha12322@gmail.com>
2023-12-30 23:39:34 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const adExitButton = document.querySelector(".btn-exit");
+const skillUpBannar = document.querySelector(".skill-up-banar");
+
+adExitButton.addEventListener("click", () => {
+  skillUpBannar.style.display = "none";
+});
